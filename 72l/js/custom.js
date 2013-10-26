25f6aa413daf70a8e34e898547e7924f2a2ff130
Vivek Dhar <vivekdhar@gmail.com>
2013-10-25 18:35:43 -0700

    updates!

--- 72l/js/custom.js
+++ 72l/js/custom.js
@@ -7,4 +7,27 @@ $(document).ready(function () {
     $('body').toggleClass('nav-open');
   });
 
+  $('#pub-header-image').anystretch();
+
+  $('.scroll-link').on('click', function(e) {
+    e.preventDefault();
+    scrollToAnchor($(this).attr('href'));
+  });
+
+  $('.show-more').on('click', function(e) {
+    e.preventDefault();
+    showMore();
+  });
+
 });
+
+function showMore() {
+  $('html,body').animate({
+    scrollTop: $(window).scrollTop() + 300
+  });
+}
+
+function scrollToAnchor(aid){
+  var aTag = $(aid);
+  $('html,body').animate({scrollTop: aTag.offset().top},'slow');
+}
